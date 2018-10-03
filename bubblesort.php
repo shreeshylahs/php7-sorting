967c22cc80d3572d11419ac9e9a1b307317edebe
shreeshylahs <xk4r50j03rx3@sg2plcpnl0251.prod.sin2.secureserver.net>
2018-10-03 08:44:44 -0700

    Bubble sort is on its way!

--- bubblesort.php
+++ bubblesort.php
@@ -1,5 +1,21 @@
-<?php 
+<?php
+    //
+    // Bubble sort algorithm
+    // Algorithm is straight forward
+    // It takes the bunch of inputs (To sort Obviously) and then rearranges that one by one in a typical way
+    // I would call it a CHILD way
+    // HERE WE GO!
+    //
   function sortit(): array{
-    return array('a', 'b', 'c');
+        //
+        // Get the input (Usually in a scaled product, input comes from the part of the application itself or the dB)
+        // But here we use a bunch of numbers as a sample
+        // You can test and download a bigger input through a file, which you can find it in the php7-sorting/bubblesort/big_input
+        //
+        $small_input = array(23, 43, 11, 7, 2, 75, 16);
+
+        // 
+        // TO BE CONTINUED
+        // 
   }
 ?>
